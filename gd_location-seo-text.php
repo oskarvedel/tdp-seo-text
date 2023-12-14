f35a26f5b604627dbf564dc33e682a0117b27a6b
oskarvedel <oskar.vedel@gmail.com>
2023-12-14 15:50:22 +0100

    removed xdebug_break(); :)))

--- gd_location-seo-text.php
+++ gd_location-seo-text.php
@@ -2,7 +2,6 @@
 
 function generate_seo_texts()
 {
-     xdebug_break();
      $geolocations = get_posts(array('post_type' => 'geolocations', 'posts_per_page' => -1));
 
      foreach ($geolocations as $geolocation) {
