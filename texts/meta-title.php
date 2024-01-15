f40c58c829d606ea4eb03df68e33a573be4fc4a0
oskarvedel <oskar.vedel@gmail.com>
2024-01-15 15:00:41 +0100

    nye meta titles

--- texts/meta-title.php
+++ texts/meta-title.php
@@ -2,7 +2,7 @@
 
 $meta_title_candidates = array(
     '15 billige Depotrum i [location] • Find Opbevaring i [location]',
-    'Sammenlign Opbevaring i [location] ([num_of_seo_gd_places] udbydere) • Find Depotrum i [location]',
+    'Sammenlign Opbevaring i [location] ([num_of_seo_gd_places] lagerhoteller) • Find Depotrum i [location]',
     '15 billige Depotrum i [location] (Fra [lowest price],-) • Find Opbevaring i [location]',
     '30 bedste Depotrum i [location] (Fra [lowest price],-) • Find Opbevaring i [location]',
 );
