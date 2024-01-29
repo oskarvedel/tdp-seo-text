8fb871202c4d5707133562c04ba7fbbbf9ebaea4
oskarvedel <oskar.vedel@gmail.com>
2024-01-29 10:11:04 +0100

    smallfix

--- tdp-seo-text-plugin.php
+++ tdp-seo-text-plugin.php
@@ -213,7 +213,7 @@ add_filter('plugin_action_links_tdp-seo-text/tdp-seo-text-plugin.php', 'add_gene
 function handle_generate_chatgpt_short_descriptions()
 {
     $subject = "test email";
-    send_email($emailoutput, $subject);
+    send_email("body", $subject);
     set_time_limit(3000);
     generate_missing_chatgpt_geolocation_short_descriptions(50);
     wp_redirect(admin_url('plugins.php?s=tdp&plugin_status=all'));
