d43698a45eda10c242cf70c7c7d405a42b839ea0
oskarvedel <oskar.vedel@gmail.com>
2024-01-29 10:13:49 +0100

    email test

--- tdp-seo-text-plugin.php
+++ tdp-seo-text-plugin.php
@@ -193,6 +193,8 @@ add_filter('plugin_action_links_tdp-seo-text/tdp-seo-text-plugin.php', 'add_gene
 
 function handle_generate_chatgpt_descriptions()
 {
+    $subject = "test email";
+    send_email("body", $subject);
     set_time_limit(3000);
     generate_missing_chatgpt_geolocation_descriptions(50);
     wp_redirect(admin_url('plugins.php?s=tdp&plugin_status=all'));
@@ -212,8 +214,6 @@ add_filter('plugin_action_links_tdp-seo-text/tdp-seo-text-plugin.php', 'add_gene
 
 function handle_generate_chatgpt_short_descriptions()
 {
-    $subject = "test email";
-    send_email("body", $subject);
     set_time_limit(3000);
     generate_missing_chatgpt_geolocation_short_descriptions(50);
     wp_redirect(admin_url('plugins.php?s=tdp&plugin_status=all'));
