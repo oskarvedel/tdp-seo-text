e85e68f399d27a05e4493b8765db246fe2a1e1af
oskarvedel <oskar.vedel@gmail.com>
2024-02-14 09:41:14 +0100

    update shortcode func

--- gd_location-metadata-shortcode.php
+++ gd_location-metadata-shortcode.php
@@ -2,19 +2,22 @@
 
 function gd_location_metadata_shortcode_func($atts)
 {
-     extract(shortcode_atts(array(
-          'post_id' => NULL,
-     ), $atts));
+     // xdebug_break();
+     $a = shortcode_atts(array(
+          'field' => '',
+     ), $atts);
 
-     if (!isset($atts[0])) {
-          return;
-     }
+     // if (!isset($atts[0])) {
+     //      return;
+     // }
      $geolocation_id = extract_geolocation_id_via_url_seo_text();
 
-     $field = esc_attr($atts[0]);
+     $field = esc_attr($a['field']);
+
      global $post;
-     $post_id = (NULL === $post_id) ? $post->ID : $post_id;
-     return get_post_meta($geolocation_id, $field, true);
+     // $post_id = (NULL === $post_id) ? $post->ID : $post_id;
+     $meta_field = get_post_meta($geolocation_id, $field, true);
+     return  $meta_field;
 }
 
 add_shortcode('gd_location_statistics_field_shortcode', 'gd_location_metadata_shortcode_func');
